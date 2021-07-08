3ac7777743889276e2e9a17068d538436899e280
長谷川 徹 <tooru.hasegawa@pasnet.local>
2021-07-08 16:14:42 +0900

    try

---
+++ try.js
@@ -0,0 +1,4 @@
+var myNumbers;
+myNumbers = [1, 2, 3, 4];
+console.log(myNumbers);
+// Output: [1, 2, 3, 4]
---
+++ try.ts
@@ -0,0 +1,4 @@
+let myNumbers: Array<number>;
+myNumbers = [1, 2, 3, 4];
+console.log(myNumbers);
+// Output: [1, 2, 3, 4]
