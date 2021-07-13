306dfa9a0a01ec978f12b41a4067181c6558b778
toru828 <82481957+toru828@users.noreply.github.com>
2021-07-13 14:58:09 +0900

    small modify for homework3

--- homework3.ts
+++ homework3.ts
@@ -1,49 +1,40 @@
 function required(message: string) {
     return (target: object, PropertyKey: string) => {
-      let value: string;
-      // The getter returns the value of the property itself.
-      const getter = function() {
-        return value;
-      };
+        let value: string;
+        const getter = function() {
+            return value;
+        };
   
-      // setter creates a random string and assign to the property value.
-      const setter = function(nam: string) {
-          if (nam.length !== 0) {
-              value = nam;
-          } else {
-              value = message;
-          }
-      }; 
-  
-      // Check https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperties
-      Object.defineProperty(target, PropertyKey, {
-        get: getter,
-        set: setter
-      }); 
+        const setter = function(nam: string) {
+            if (nam.length !== 0) {
+                value = nam;
+            } else {
+                value = message;
+            }
+        }; 
+
+        Object.defineProperty(target, PropertyKey, {
+            get: getter,
+            set: setter
+        }); 
     };
-  }
-  
-  class Person {
+}
+
+class Person {
     @required("Name is required!")
     name: string;
   
     constructor(name: string) {
-      this.name = name;
+        this.name = name;
     }
   
     show() {
-      console.log(this.name)
+        console.log(this.name)
     }
-  }
+}
   
-  const firstPerson = new Person("John");
-  firstPerson.show();
-  // Output: "John" 
-  
-  const secondPerson = new Person("");
-  secondPerson.show();
-  /* Output: 
-  "Name is required" 
-  "" 
-  */
-  
+const firstPerson = new Person("John");
+firstPerson.show();
+
+const secondPerson = new Person("");
+secondPerson.show();
