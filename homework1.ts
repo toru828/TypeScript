add8b882f41fb395f6ab7000fdc535949dac6751
toru828 <82481957+toru828@users.noreply.github.com>
2021-07-15 20:32:05 +0900

    Update homework1.ts

--- homework1.ts
+++ homework1.ts
@@ -8,7 +8,16 @@ function countStringType(array: string[] | number[]) {
     console.log(totalNumber);
 }
 
-let arr: any
+let arr: any;
+let arr1: any;
+let arr2: any;
+let arr3: any;
 arr = [1, "h", 2, "k" , "hello"];
+arr1 = [1, 3, 2, 7, 5, 9];
+arr2 = ["tom", "john", "nancy", "jesica", "watson"];
+arr3 = ["aaa", 1, 1, 4, 23, 23, 3];
 
 countStringType(arr);
+countStringType(arr1);
+countStringType(arr2);
+countStringType(arr3);
